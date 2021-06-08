d144fae1e0facc11604ba52e5643309c5f481e62
kangsyahrul <syah.hidayat71@gmail.com>
2021-06-08 16:30:44 +0700

    Revert "Revert "Update Readme""
    This reverts commit e92f231627d1380def76b8666ec7344ab731b103.

--- main.py
+++ main.py
@@ -79,9 +79,8 @@ print('Initialize Done!')
 # FUNCTIONS
 def contrastive_loss(logits1, logits2, label, margin, eps=1e-7):
     Dw = tf.sqrt(eps + tf.reduce_sum(tf.square(logits1 - logits2), 1))
-    loss = tf.reduce_mean((1. - tf.cast(label, tf.float32))
-                          * tf.square(Dw) + tf.cast(label, tf.float32)
-                          * tf.square(tf.maximum(margin - Dw, 0)))
+    loss = tf.reduce_mean((1. - tf.cast(label, tf.float32)) * tf.square(Dw)
+                          + tf.cast(label, tf.float32) * tf.square(tf.maximum(margin - Dw, 0)))
     return loss, Dw
 
 
